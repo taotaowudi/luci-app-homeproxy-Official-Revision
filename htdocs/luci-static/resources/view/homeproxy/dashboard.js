3ba88e0d04beb154f4d2e8e4586a86f522438669
taotaowudi <whtglj@gmail.com>
2026-09-16 11:39:21 +0800

    面板优化

--- htdocs/luci-static/resources/view/homeproxy/dashboard.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-'require view';
-
-return view.extend({
-	render: function() {
-		window.location.href = '/luci-static/resources/homeproxy/zashboard/index.html';
-		return E([]);
-	}
-});
